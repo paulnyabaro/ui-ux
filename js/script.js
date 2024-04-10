4604a1c28d5320063fad51055d2e8bd6d2af3b8b
paulnyabaro <paulnyabaro1@gmail.com>
2024-04-10 07:25:22 +0300

    Working on the dots

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ setInterval(() =>{
     let mm = document.getElementById('mm');
     let ss = document.getElementById('ss');
 
+    let sec_dot = document.querySelector('.sec_dot');
+    let min_dot = document.querySelector('.min_dot');
+    let hr_dot = document.querySelector('.hr_dot');
+
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
@@ -12,4 +16,9 @@ setInterval(() =>{
     hh.style.strokeDashoffset = 510 - (510 * h) / 12;
     mm.style.strokeDashoffset = 510 - (630 * m) / 60;
     ss.style.strokeDashoffset = 510 - (760 * s) / 60;
+
+    // 360 / 6
+    sec_dot.style.transform = `rotateZ(${s * 6}deg)`;
+    min_dot.style.transform = `rotateZ(${m * 6}deg)`;
+    hr_dot.style.transform = `rotateZ(${h * 30}deg)`;
 });
